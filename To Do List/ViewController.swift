f2c000658106b354e877aaa8184d1df842ac28c5
JohnKostanov <kostanovd@icloud.com>
2019-08-09 20:54:21 +0200

    Initial Commit

---
+++ To Do List/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  To Do List
+//
+//  Created by  Джон Костанов on 09/08/2019.
+//  Copyright © 2019 John Kostanov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
