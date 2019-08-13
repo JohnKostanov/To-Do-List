945e5c08b087bc29f3611950e031de7418693d2f
JohnKostanov <kostanovd@icloud.com>
2019-08-13 17:01:05 +0200

    Add numberOfSections in ToDoItemTableViewController

--- To Do List/View Controllers/ToDoItemTableViewController.swift
+++ To Do List/View Controllers/ToDoItemTableViewController.swift
@@ -18,3 +18,33 @@ class ToDoItemTableViewController: UITableViewController {
     }
 
 }
+//MARK: - UITableViewDataSource
+extension ToDoItemTableViewController/*: UITableViewDataSource*/ {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return todo.keys.count
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let value = todo.values[section]
+        return value is Date ? 2 : 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section = indexPath.section
+        let value = todo.values[section]
+        let cell = getCellFor(indexPath: indexPath, with: value)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let key = todo.capitilizedKeys[section]
+        return key
+    }
+}
+
+// MARK: - Cell Configurator
+extension ToDoItemTableViewController {
+    func getCellFor(indexPath: IndexPath, with value: Any?) -> UITableViewCell {
+        
+        return UITableViewCell()
+    }
+}
